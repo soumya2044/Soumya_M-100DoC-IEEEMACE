068a666f560fd5c253a7ec4132201840c1f3faae
soumya2044 <126675192+soumya2044@users.noreply.github.com>
2023-03-26 10:03:42 +0530

    Update week1_task1.py

--- week1_task1.py
+++ week1_task1.py
@@ -1,17 +1,13 @@
-input_str=str(input("enter the string: "));
-
-# defined a fn l2n to do the task
-
+input_strng=str(input("enter the string: "));
+# defined a fn l2n to return the digit
 def l2n(letter):
    S="abcdefghijklmnopqrstuvwxyz";
    return S.index(letter)+1;
-
-output_str="";
-
-for i in input_str:
-    if i.isalpha():
-      j=i.lower();
-      ouput_str=output_str+" "+output_str(l2n(j));
+output_stng="";
+for i in input_strng:
+    if i.isalpha():        
+      j=i.lower();             #for checking both uppercase and lowercase
+      output_stng=output_stng+" "+str(l2n(j));
     else:
       continue
-print(str(output_str));
+print(str(output_stng));
